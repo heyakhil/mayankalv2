37789581de00a841e9eef64b30995ff70be735b3
heyakhil <54666562+heyakhil@users.noreply.github.com>
2020-01-22 21:01:27 +0530

    New Update was there
    Simple a text will be sent

--- forgotprocess.php
+++ forgotprocess.php
@@ -8,12 +8,8 @@
 				
                 $to = $email;
 				$subject = "Mayankal Account Verification";
-				$message = "<html>
-							<body>
-								Hello User! Your Reset Password link:<br><a href='http://localhost/project/mayankalv2/confirmpassword.php?email=".<?php $email; ?>.">Click Here</a> <br> Here you can easily Reset Your password.....
-							</body>
-							</html>" ;
-				$from = "sumitkumar993618@gmail.com";
+				$message = "Hello User! Your Reset Password link: https://localhost/project/mayankalv2/confirmpassword.php?email=".<?php $email; ?>.">Click Here</a> <br> Here you can easily Reset Your password.....";
+				$from = "team@mayankal.ml";
 				$headers = "From:" . $from;
 				if(mail($to,$subject,$message,$headers)){
                 ?>
