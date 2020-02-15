1b263a334ac1832e46ca862ebce0cb6a96079e34
farhan3040 <52371417+farhan3040@users.noreply.github.com>
2020-02-16 00:04:30 +0530

    upda
    update

--- dashboard/WriteContent.php
+++ dashboard/WriteContent.php
@@ -15,26 +15,27 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <!--<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">-->
   <script type="text/javascript" src="../js/ckeditor/ckeditor.js"></script>
+
 </head>
-<body>
+<body style="background-color:#F3ECEC;">
   
 <div class="row">
-  <div class="col-sm-8 col-md-8">
-    <div class="form-group" style="margin: 10px;">
+  <div style="margin-right:500px; margin-left:250px;">
+    <div class="form-group" style="margin: 20px;">
     <form action="../assets/write_post.php" method="POST">
       <h3>Title</h3>
       <input type="text" class="form-control" id="usr" placeholder="Enter the title" name="title">
       <hr>
-    <textarea class="ckeditor" name="editor" style="height:100%; margin-left: 2px;"></textarea><br> 
+    <textarea class="ckeditor" name="editor" style="height:200px;"></textarea><br> 
     <input type="hidden" name="order_id" value="<?php echo $or_id; ?>">
     <input type="hidden" name="customer" value="<?php echo $customer; ?>">
-    <input type="submit" class="btn btn-primary" value="Submit" name="submit" style="margin-left: 8px;">
+    <input type="submit" class="btn btn-primary" value="Submit" name="submit" style="width:100px;">
   </form>
 </div>
-  </div>
-  <div class="col-sm-4 col-md-4" style="border-left: 2px solid black; height: 700px;">
+</div>
+  <!-- <div class="col-sm-4 col-md-4" style="border-left: 2px solid black; height: 700px;"> -->
 
-  <?php 
+  <!-- <?php 
 
   $sql = "SELECT * FROM orders WHERE `order_id`='$or_id'";
   $result = mysqli_query($conn, $sql);
@@ -55,7 +56,7 @@
   }
 
 
-   ?>
+   ?> -->
     
   </div>
  
