6a235d9cf891a7508d222e82f9fb4d30d4f86718
farhan3040 <52371417+farhan3040@users.noreply.github.com>
2020-02-16 22:03:12 +0530

    update
    update

--- dashboard/WriteContent.php
+++ dashboard/WriteContent.php
@@ -15,13 +15,16 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <!--<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">-->
   <script type="text/javascript" src="../js/ckeditor/ckeditor.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 </head>
 <body style="background-color:#F3ECEC;">
   
-<div class="row">
-  <div style="margin-right:500px; margin-left:250px;">
-    <div class="form-group" style="margin: 20px;">
+<div class="fluid-container">
+  <div style="margin-right:500px; margin-left:200px;">
+    <div class="form-group" class="col-xm-10">
     <form action="../assets/write_post.php" method="POST">
       <h3>Title</h3>
       <input type="text" class="form-control" id="usr" placeholder="Enter the title" name="title">
@@ -31,10 +34,40 @@
     <input type="hidden" name="customer" value="<?php echo $customer; ?>">
     <input type="submit" class="btn btn-primary" value="Submit" name="submit" style="width:100px;">
   </form>
-</div>
-</div>
-  <!-- <div class="col-sm-4 col-md-4" style="border-left: 2px solid black; height: 700px;"> -->
+<!-- </div> -->
+  <!-- <div class="col-sm-4 col-md-4" style="height: 700px;"> -->
 
+<!-- right-side -->
+
+ <div class="container" style="width:300px;">
+  <div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Collapsible Group 2</a>
+        </h4>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Collapsible Group 3</a>
+        </h4>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+      </div>
+    </div>
+  </div> 
+</div>
+ <!-- End Right-side -->
+ </div>
   <!-- <?php 
 
   $sql = "SELECT * FROM orders WHERE `order_id`='$or_id'";
@@ -58,9 +91,9 @@
 
    ?> -->
     
-  </div>
- 
+  <!-- </div> -->
 
-  
+
+  </div>
 </body>
 </html>
